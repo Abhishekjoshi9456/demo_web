9a1947bf4cd48dc184e8582e1b653f45b19958e1
Abhishekjoshi9456 <113884194+Abhishekjoshi9456@users.noreply.github.com>
2024-07-15 12:37:13 +0530

    Add files via upload

---
+++ form_design.py
@@ -0,0 +1,17 @@
+# form design
+import streamlit as st
+
+
+name = st.text_input("Enter Your Name : ")
+fname = st.text_input("Enter your Father Name : ")
+adr = st.text_area("Enter your Text : ")
+classdata = st.selectbox("Enter Your Class :", (1,2,3,4,5,6))
+
+button = st.button("Done")
+if button:
+    st.markdown(f"""
+                Name : {name}
+                Father name : {fname}
+                Address : {adr}
+                class : {classdata}
+                """)
